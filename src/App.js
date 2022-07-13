71a2f67de7e5ceb4983a70e034b65c53b1be3666
BharAni <76902986+Bharaniarasu@users.noreply.github.com>
2022-07-13 13:43:32 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import styles from "./styles.css";
+import styled from "styled-components";
+export default function App() {
+  // const Content = styled.div`
+  //   width: 50%;
+  //   color: red;
+
+  //   @media (max-width: 768px) {
+  //     width: auto;
+  //   }
+  //   &:hover {
+  //     color: blue;
+  //   }
+
+  //   & .data {
+  //     background: ${(props) => props.bg};
+  //   }
+  // `;
+
+  return <div className="App"></div>;
+}
